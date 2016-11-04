faf05c6986a1d4bc778e798f84e194a7269d2c7f
Xavier Mol <xavier.mol@kit.edu>
2016-11-04 10:10:44 +0100

    The template should not be configurable.

--- manifests/create_digauthfile.pp
+++ manifests/create_digauthfile.pp
@@ -1,12 +1,11 @@
 define xrootd::create_digauthfile (
-  $template = $xrootd::config::digauthfile_template,
   $host,
   $group,
 ) {
   require xrootd::config
 
   file {$title:
-    content => template($template)
+    content => template($xrootd::config::digauthfile_template)
   }
 
 }
