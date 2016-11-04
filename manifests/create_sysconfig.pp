e1cf883bf3f8a31b6d3da97c264f48ff2cc49bb2
Xavier Mol <xavier.mol@kit.edu>
2016-11-04 10:54:14 +0100

    Inherit the user and group parameter from the config class (otherwise non-default settings are not picked up).

--- manifests/create_sysconfig.pp
+++ manifests/create_sysconfig.pp
@@ -1,6 +1,6 @@
 define xrootd::create_sysconfig (
-  $xrootd_user = $xrootd::params::xrootd_user,
-  $xrootd_group = $xrootd::params::xrootd_group,
+  $xrootd_user = $xrootd::config::xrootd_user,
+  $xrootd_group = $xrootd::config::xrootd_group,
   $xrootd_instances_options = $xrootd::params::xrootd_instances_options,
   $cmsd_instances_options = $xrootd::params::cmsd_instances_options,
   $purd_instances_options = $xrootd::params::purd_instances_options,
