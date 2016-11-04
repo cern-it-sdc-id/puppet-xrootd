01abed0a396c650ab11462698f8152d627a56915
Xavier Mol <xavier.mol@kit.edu>
2016-11-04 11:08:05 +0100

    Add parameters to the resource type, that are evaluated by the template.

--- manifests/create_systemd.pp
+++ manifests/create_systemd.pp
@@ -1,8 +1,12 @@
 define xrootd::create_systemd (
+  $xrootd_user = $xrootd::config::xrootd_user,
+  $xrootd_group = $xrootd::config::xrootd_group,
   $exports = $xrootd::params::exports,
   $daemon_corefile_limit = $xrootd::params::daemon_corefile_limit,
+  $enable_hdfs = false,
+  $java_home = undef,
 ) {
-  require xrootd::config
+  require xrootd::service
 
   file {"/etc/systemd/system/${title}.service.d/":
     ensure => directory,
